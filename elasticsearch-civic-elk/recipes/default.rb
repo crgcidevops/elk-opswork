df5678059f8abf4388c9dfca1157efe2a41aaefe
mihasK <mihas-91@tut.by>
2017-03-30 23:06:51 +0300

    include java recipe

--- elasticsearch-civic-elk/recipes/default.rb
+++ elasticsearch-civic-elk/recipes/default.rb
@@ -4,7 +4,29 @@
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
 
-include_recipe 'java'
+
+apt_repository "webupd8team" do
+  uri "http://ppa.launchpad.net/webupd8team/java/ubuntu"
+  components ['main']
+  distribution node['lsb']['codename']
+  keyserver "keyserver.ubuntu.com"
+  key "EEA14886"
+  deb_src true
+
+end
+
+# could be improved to run only on update
+execute "accept-license" do
+  command "echo oracle-java7-installer shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections"
+end
+
+package "oracle-java7-installer" do
+  action :install
+end
+
+package "oracle-java7-set-default" do
+  action :install
+end
 
 elasticsearch_user 'elasticsearch'
 
