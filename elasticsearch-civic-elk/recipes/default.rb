6678a27095d18b2c8ed6969975ddcee0caaaccc1
mihasK <mihas-91@tut.by>
2017-03-31 13:49:49 +0300

    discovery-ec2 plugin

--- elasticsearch-civic-elk/recipes/default.rb
+++ elasticsearch-civic-elk/recipes/default.rb
@@ -17,20 +17,42 @@ end
 elasticsearch_configure 'elasticsearch' do
   allocated_memory '512m'
   configuration ({
-  'cluster.name' => 'mycluster',
-  'node.name' => 'node01',
-  'http.cors.enabled' => true,
-  'http.cors.allow-origin' => '/https?:\/\/localhost(:[0-9]+)?/',
-  'http.cors.allow-credentials' => true,
-  'http.cors.allow-headers' => 'X-Requested-With,X-Auth-Token,Content-Type, Content-Length, Authorization'
+    'cluster.name' => 'mycluster',
+    #'node.name' => 'node01',
+    'network.host' => '0.0.0.0',
+
+    'http.cors.enabled' => true,
+    'http.cors.allow-origin' => '/https?:\/\/localhost(:[0-9]+)?/',
+    'http.cors.allow-credentials' => true,
+    'http.cors.allow-headers' => 'X-Requested-With,X-Auth-Token,Content-Type, Content-Length, Authorization',
+    'cloud' => {
+      'aws' => {
+          'access_key' => 'XXX',
+          'secret_key' => 'XXX'
+        }
+      },
+
+    'discovery' =>  {'zen.hosts_provider' => 'ec2' },
+
+    'cloud.node.auto_attributes' => true,
+    'cluster.routing.allocation.awareness.attributes' => 'aws_availability_zone',
+    'discovery.ec2.groups' => 'elasticsearch'
+
   })
 end
 
-elasticsearch_service 'elasticsearch' do
-  #service_actions [:enable, :start]
-end
+
 
 #elasticsearch_plugin 'head' do
 #  url 'mobz/elasticsearch-head'
 #  notifies :restart, 'elasticsearch_service[elasticsearch]', :delayed
 #end
+
+elasticsearch_plugin 'discovery-ec2' do
+  url 'discovery-ec2'
+  #notifies :restart, 'elasticsearch_service[elasticsearch]', :delayed
+end
+
+elasticsearch_service 'elasticsearch' do
+  #service_actions [:enable, :start]
+end
