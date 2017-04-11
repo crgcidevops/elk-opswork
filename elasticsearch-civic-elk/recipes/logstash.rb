c2790e70aad8deed50f4907b39ce001d52982438
mihasK <mihas-91@tut.by>
2017-04-11 18:57:47 +0300

    yum logstash

--- elasticsearch-civic-elk/recipes/logstash.rb
+++ elasticsearch-civic-elk/recipes/logstash.rb
@@ -1,13 +1,30 @@
-docker_service 'default' do
-  action [:create, :start]
+# docker_service 'default' do
+#   action [:create, :start]
+# end
+
+# docker_image 'busybox' do
+#   action :pull
+# end
+
+# docker_container 'an-echo-server' do
+#   repo 'busybox'
+#   port '1234:1234'
+#   command "nc -ll -p 1234 -e /bin/cat"
+# end
+
+include_recipe 'java'
+
+yum_repository 'elastic5' do
+  description "Elastic repository for 5.2 packages"
+  baseurl "https://artifacts.elastic.co/packages/5.2/yum"
+  gpgkey "https://artifacts.elastic.co/GPG-KEY-elasticsearch"
+  enabled true
+  autorefresh true
+  gpgcheck true
+  type "rpm-md"
+  action :create
 end
 
-docker_image 'busybox' do
-  action :pull
-end
 
-docker_container 'an-echo-server' do
-  repo 'busybox'
-  port '1234:1234'
-  command "nc -ll -p 1234 -e /bin/cat"
-end
+
+yum_package 'logstash'
