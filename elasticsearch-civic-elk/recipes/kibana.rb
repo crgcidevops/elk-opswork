8f8d4c0e933b9b999ffe60879462036e9c3631c9
mihasK <mihas-91@tut.by>
2017-04-18 13:52:23 +0300

    kibana

--- elasticsearch-civic-elk/recipes/kibana.rb
+++ elasticsearch-civic-elk/recipes/kibana.rb
@@ -4,26 +4,29 @@
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
 
-kibana5_install 'kibana' do
-  version '5.2.2'
+include_recipe 'java'
+
+yum_repository 'elastic5' do
+  description "Elastic repository for 5.x packages"
+  baseurl "https://artifacts.elastic.co/packages/5.x/yum"
+  gpgkey "https://artifacts.elastic.co/GPG-KEY-elasticsearch"
+  enabled true
+  gpgcheck true
+  action :create
 end
 
-kibana5_configure 'kibana' do
-  configuration 'server.port' => 5601,
-                'server.host' => 'localhost',
-                'elasticsearch.url' => 'http://localhost:9200',
-                'logging.dest' => '/var/log/kibana.log'
-end
+yum_package 'kibana'
 
-node.default['nginx']['default_site_enabled'] = false
-include_recipe 'chef_nginx'
 
-nginx_site 'kibana' do
-  template 'nginx-site.conf.erb'
-  variables(
-    listen_address: '0.0.0.0',
-    listen_port: '80',
-    server_name: 'kibana.test',
-    kibana_port: '5601'
-  )
-end
+#node.default['nginx']['default_site_enabled'] = false
+#include_recipe 'chef_nginx'
+
+#nginx_site 'kibana' do
+#  template 'nginx-site.conf.erb'
+#  variables(
+#    listen_address: '0.0.0.0',
+#    listen_port: '80',
+#    server_name: 'kibana.test',
+#     kibana_port: '5601'
+#   )
+# end
