f4684f836475f42354ebad8ee7de49598964ab1d
mihas <mkoypish@gmail.com>
2017-04-22 21:57:20 +0300

    fix

--- elasticsearch-civic-elk/recipes/logstash_receiver.rb
+++ elasticsearch-civic-elk/recipes/logstash_receiver.rb
@@ -1,16 +1,3 @@
-# docker_service 'default' do
-#   action [:create, :start]
-# end
-
-# docker_image 'busybox' do
-#   action :pull
-# end
-
-# docker_container 'an-echo-server' do
-#   repo 'busybox'
-#   port '1234:1234'
-#   command "nc -ll -p 1234 -e /bin/cat"
-# end
 
 include_recipe 'java'
 
@@ -26,17 +13,6 @@ end
 
 
 yum_package 'logstash'
-
-# service "logstash" do
-#   action :start
-#   reload_command  "initctl reload logstash"
-#   restart_command "initctl restart logstash"
-# #  service_name               String # defaults to 'name' if not specified
-#   start_command "initctl start logstash"
-#   status_command "initctl status logstash"
-#   stop_command "initctl stop logstash"
-
-# end
 
 
 template '/etc/logstash/conf.d/logstash_receiver.conf' do
